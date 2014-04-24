e093496eccfb10b940bcbe7c236e54d8bae188d2
Geoffrey Unger <geoffunger@gmail.com>
2014-04-23 21:25:01 -0700

    Removed alert on Parse.com save

--- main.js
+++ main.js
@@ -11,5 +11,5 @@ Parse.initialize(appID,javascriptKey);
 var TestObject = Parse.Object.extend("TestObject");
 var testObject = new TestObject();
 testObject.save({foo: "bar"}).then(function(object) {
-    alert("yay! it worked");
+    
 });
