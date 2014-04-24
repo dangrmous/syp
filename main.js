302c20bcf91888b68471ab94fdfc1d4c0ca141c0
Geoffrey Unger <geoffunger@gmail.com>
2014-04-23 22:18:14 -0700

    Got JSON and .png signature file saving

--- main.js
+++ main.js
@@ -3,13 +3,18 @@
  */
 
 $(document).ready(function() {
-    $('.sigPad').signaturePad({drawOnly:true});
+    pad = $('.sigPad').signaturePad({drawOnly:true});
+    Parse.initialize(appID,javascriptKey);
+
+    var TestObject = Parse.Object.extend("TestObject");
+    var testObject = new TestObject();
+    $("#submit").click(function(){
+        sigImageData = pad.getSignatureImage();
+        file = new Parse.File("signatureImage.png", { base64: sigImageData });
+        testObject.save({'signature': $('input[name="output"]').val(), 'signatureImage': file});
+    });
+
 });
 
-Parse.initialize(appID,javascriptKey);
 
-var TestObject = Parse.Object.extend("TestObject");
-var testObject = new TestObject();
-testObject.save({foo: "bar"}).then(function(object) {
-    
-});
+
