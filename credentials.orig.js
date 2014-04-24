fb147c181cd601326991c8e7c8204f7b2b439995
Geoff Unger <geoff@shopigniter.com>
2014-04-24 10:24:55 -0700

    Added a credentials orig file

---
+++ credentials.orig.js
@@ -0,0 +1,10 @@
+/**
+ * Created by geoff on 4/22/14.
+ */
+
+/*
+* Rename this file to credentials.js and place it one directory above the other files (usually one dir above web root)
+ */
+
+appID = "YOUR-PARSE-APP-ID";
+javascriptKey = "YOUR-PARSE-JAVASCRIPT-KEY";
